6925e74072d274c67783d7d88c0b3ec5737c5c31
Amit Kumar <1670365+amitkumar3968@users.noreply.github.com>
2020-05-25 11:43:48 +0530

    adding new files

---
+++ paytm/paytm_config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  paytm_config: {
+		MID: 'ppTtAL55993483278240',
+		WEBSITE: 'WEBSTAGING',
+    CHANNEL_ID: 'WAP',
+    INDUSTRY_TYPE_ID: 'Retail',
+    MERCHANT_KEY : 'a!jo!aUihJ0oeUSv'
+	}
+}
